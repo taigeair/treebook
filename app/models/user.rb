29d927d147fb77d1f52986b50ee88cd2d9554e14
Taige Zhang <taigezhang@gmail.com>
2013-07-10 14:36:47 -0400

    installed simple_form gem, created relationships, added dropdown of all users, and started adding nav links for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :first_name, :last_name, :profile_name, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  
+  has_many :statuses
+  
+  def full_name
+    first_name  + " " + last_name
+  end
 end
