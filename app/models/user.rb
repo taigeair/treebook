98a701d7ea3fea4b25ea20c202a3b65a19791424
Taige Zhang <taigezhang@gmail.com>
2013-07-12 16:23:38 -0400

    added user sign up tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,14 @@ class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :profile_name, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
   
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true,
+                           uniqueness: true,
+                           format: {
+                              with: /a-zA-Z0-9_-/,
+                              message: 'Must be formatted correctly.'
+                           }
   has_many :statuses
   
   def full_name
