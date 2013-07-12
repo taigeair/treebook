1cb75db271390abbfa45a00cdfa366fddee902a4
Taige Zhang <taigezhang@gmail.com>
2013-07-12 17:17:04 -0400

    modified flash messaging system to use bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   validates :profile_name, presence: true,
                            uniqueness: true,
                            format: {
-                              with: /a-zA-Z0-9_-/,
+                              with: /a-zA-Z\-\_+/,
                               message: 'Must be formatted correctly.'
                            }
   has_many :statuses
