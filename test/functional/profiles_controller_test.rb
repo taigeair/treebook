625297d836fc054f926d5c5c604b69bb8589bc32
Taige Zhang <taigezhang@gmail.com>
2013-07-18 17:56:18 -0400

    some things weren't being saved to github for some reason.
    This is a bit confusing. For example I can't find the profiles directory in views.

---
+++ test/functional/profiles_controller_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class ProfilesControllerTest < ActionController::TestCase
+  test "should get show" do
+    get :show, id: users(:jason).profile_name
+    assert_response :success
+    assert_template 'profiles/show'
+  end 
+
+  test "should render a 404 on profile not found" do
+    get :show, id: "doesn't exist"
+    assert_response :not_found
+  end
+  
+  test "that variable are assigned on successful profile viewing" do
+    get :show, id: users(:jason).profile_name
+    assert assigns(:user)
+    assert_not_empty assigns(:statuses)
+  end  
+end
