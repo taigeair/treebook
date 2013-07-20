fc00fb23374e2f6d5aedfa63d0e3f72b894de1c8
Taige Zhang <taigezhang@gmail.com>
2013-07-20 17:41:06 -0400

    added profile pics to statuses using gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,12 @@ class User < ActiveRecord::Base
   def full_name
     first_name  + " " + last_name
   end
+  
+  def gravatar_url
+    stripped_email = email.strip
+    lowercase_email = stripped_email.downcase
+    hash = Digest::MD5.hexdigest(lowercase_email)
+    
+    "http://www.gravatar.com/avatar/#{hash}?s=100"
+  end
 end
